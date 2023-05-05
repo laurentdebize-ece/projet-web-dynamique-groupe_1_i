6acebdd778d50fac9b2a351daef3eb08b1f864df
Guillaume dse <guillaume.desaintetienne@edu.ece.fr>
2023-05-05 13:50:06 +0200

    ajout index

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Mes Compétences</title>
+    <link href="style1.css" rel="stylesheet" type="text/css"/>
+    <link rel= "stylesheet"
+          href= "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src= "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.j s"> </script>
+</head>
+<body>
+
+<header>
+    <h2>ECE</h2>
+</header>
+
+<section>
+    <nav>
+        <div id = "cercle"></div>
+        </br></br></br></br>
+        <ul>
+            <li><a href="#">Matiere</a></li></br>
+            <li><a href="#">Compétences</a></li></br>
+            <li><a href="#">Compétences transverses</a></li></br>
+            <li><a href="#">Toutes les compétences</a></li>
+        </ul>
+    </nav>
+
+    <article>
+        <h1>Accueil</h1>
+    </article>
+</section>
+
+<footer>
+    <p>Footer</p>
+</footer>
+
+</body>
+</html>

6fbb26f13236bd7056b20e839ffdb212889ac01c
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2023-05-12 12:48:08 +0200

    -a

--- PHP/PHP/ajout_utilisateur.php
+++ PHP/PHP/ajout_utilisateur.php
@@ -32,3 +32,39 @@ try {
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+</head>
+<body>
+    <h1>Inscription</h1>
+    <form action="ajout_utilisateur.php" method="post">
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required>
+        <br>
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" required>
+        <br>
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <label for="mot_de_passe">Mot de passe :</label>
+        <input type="password" id="mot_de_passe" name="mot_de_passe" required>
+        <br>
+        <label for="type">Type :</label>
+        <select id="type" name="type" required>
+            <option value="">--Choisissez une option--</option>
+            <option value="etudiant">Étudiant</option>
+            <option value="professeur">Professeur</option>
+        </select>
+        <br>
+        <button type="submit">Inscription</button>
+    </form>
+</body>
+</html>
